8a40f94f983223b730524035f992d0ae95481293
Jose Gustavo Abreu Murta <jgustavoam@gmail.com>
2023-01-13 21:43:28 -0300

    Update main.go

--- Golang/Read bin files/main.go
+++ Golang/Read bin files/main.go
@@ -15,27 +15,28 @@ import (
 
 func main() {
 
-	f, err := os.Open("basic-4000_commodore128.bin")
+	f, err := os.Open("basic-4000_commodore128.bin") // open binary file
 
-	if err != nil {
-		log.Fatal(err)
+	if err != nil { // if any error
+		log.Fatal(err) // print error message
 	}
 
-	defer f.Close()
+	defer f.Close() // close file at end
 
 	reader := bufio.NewReader(f)
-	buf := make([]byte, 16384)
+	buf := make([]byte, 16384) // create buffer  16384 bytes
 
 	for {
-		_, err := reader.Read(buf)
+		_, err := reader.Read(buf) // reaf file into buffer
 
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println(err)
+		if err != nil { // if any error
+			if err != io.EOF { // except end of file
+				fmt.Println(err) // print error message
 			}
 			break
 		}
 
-		fmt.Printf("%s", hex.Dump(buf))
+		fmt.Printf("%s", hex.Dump(buf)) // print buffer at console
 	}
 }
+
